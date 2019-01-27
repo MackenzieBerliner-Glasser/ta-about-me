cf70cd469c974aff182c9eba58413f3b0c84fe5b
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-01-27 12:53:01 -0800

    refactor for conditional

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const quizForm = document.getElementById('quiz-form');
 const resultTrue = document.getElementById('result-true');
+const resultFalse = document.getElementById('result-false');
 
 quizForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    const pickTrue = quizForm.elements.true.value;
-    const pickFalse = quizForm.elements.false.value;
+    const pickAnswer = quizForm.elements.answer.value;
 
-    if(pickTrue) {
+    if(pickAnswer === 'true') {
         resultTrue.classList.remove('hidden');
     } else {
-        resultTrue.classList.remove('hidden');
+        resultFalse.classList.remove('hidden');
     }
 });
